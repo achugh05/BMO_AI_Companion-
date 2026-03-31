0ddf4cdcc29ad15d8b67edfbfd02849903574619
ryanstupie <ryanchengwastaken@gmail.com>
2026-03-30 19:50:49 -0700

    Deleted access token :sob:

--- llm/canvas/canvas_setup.py
+++ llm/canvas/canvas_setup.py
@@ -4,9 +4,9 @@ from canvasapi import Canvas
 from zoneinfo import ZoneInfo
 
 #Canvas Domain
-API_URL = 'https://canvas.ubc.ca'
+API_URL = ''
 # Canvas API key/token
-API_KEY = '11224~CLBLaAYeUJVBTXaDefT8c9Q36eWxBBunUXmK9fkXCknXBGUnBy4YrcMxhJAVATue'
+API_KEY = ''
 canvas = Canvas(API_URL, API_KEY)
 user = canvas.get_current_user()
 courses = user.get_courses(enrollment_state='active')
@@ -16,8 +16,8 @@ for course in courses:
 
     if hasattr(course, 'name'):
         #Take course code
-        course_code = course.name[:10]
-        clean_course_code = course_code.upper().replace("_O", "").replace(" ", "")
+        course_code = course.name
+        clean_course_code = course_code.upper().replace(" ", "")
         coursedict[clean_course_code] = course
 
 
